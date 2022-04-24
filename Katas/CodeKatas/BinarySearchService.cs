e052f8749bac958e6fb4b16dd0dce4aa331551f8
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-04-24 22:46:41 +0100

    Firstr

---
+++ Katas/CodeKatas/BinarySearchService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Katas.CodeKatas
+{
+    public sealed class BinarySearchService
+    {
+        /** 
+         * The binary Chop is used to sort through an ordered list 
+         * and find the position of a target element. 
+         * 
+         * This example takes a list of integers, allows the user to find the 
+         * index of a given value in the list. 
+         * 
+         * If it can't be found then return -1
+         */
+        public int ChopIterative(List<int> sortedList, int value)
+        {
+            if (sortedList.Count == 0) { return -1; }
+
+            var minNumber = 0;
+            var maxNumber = sortedList.Count - 1;
+            
+            while (minNumber <= maxNumber)
+            {
+                var middleValue = (minNumber + maxNumber) / 2;
+
+                if (sortedList[middleValue] == value)
+                {
+                    return middleValue;
+                }
+
+                if (sortedList[middleValue] > value)
+                {
+                    maxNumber = middleValue - 1;
+                }
+
+                if (sortedList[middleValue] < value)
+                {
+                    minNumber = middleValue + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Tests/BinarySearch/GivenABinaryChopOnASortedArrayOfIntegersUsingTheIterativeApproach.cs
@@ -0,0 +1,47 @@
+﻿using Katas.CodeKatas;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+/** http://codekata.com/kata/kata02-karate-chop/ */
+
+namespace Tests
+{
+    [TestFixture]
+    class GivenABinaryChopOnASortedArrayOfIntegersUsingTheIterativeApproach
+    {
+
+        private BinarySearchService _subject;
+
+        [SetUp]
+        public void Setup()
+        {
+            _subject = new BinarySearchService();
+        }
+
+        [Test]
+        public void ThenTheCorrectIndexIsReturnedForTheRequestedItem()
+        {
+            Assert.AreEqual(_subject.ChopIterative(new List<int>(), 3), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { -1 }, 3), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1 }, 1), 0);
+
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5 }, 1), 0);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5 }, 3), 1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5 }, 5), 2);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5 }, 0), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5 }, 2), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5 }, 4), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5 }, 6), -1);
+
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 1), 0);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 3), 1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 5), 2);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 7), 3);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 0), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 2), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 4), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 6), -1);
+            Assert.AreEqual(_subject.ChopIterative(new List<int> { 1, 3, 5, 7 }, 8), -1);
+        }
+    }
+}
